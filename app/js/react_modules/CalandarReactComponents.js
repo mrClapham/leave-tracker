a60f4ecc3a7d47f7cfe4f6ca6de7611090c22de9
Graham Clapham <graham.clapham@thomsonreuters.com>
2015-02-10 14:24:21 +0000

    The cells auto filling on instatiation.

--- app/js/react_modules/CalandarReactComponents.js
+++ app/js/react_modules/CalandarReactComponents.js
@@ -1,28 +1,45 @@
 /** @jsx React.DOM */
 var Calendar = React.createClass({displayName: "Calendar",
     propTypes: {
-        allData : React.PropTypes.array,
+        alldata : React.PropTypes.array,
         dates : React.PropTypes.array.isRequired,
         sublist : React.PropTypes.array,
         daysoff : React.PropTypes.array,
-        sublistfunc : React.PropTypes.func
+        sublistfunc : React.PropTypes.func,
+        worker : React.PropTypes.string,
+        key:React.PropTypes.number
+    },
+
+    dataByDate: function(array, value ){
+        return _.filter(array, function(n){
+            if(!n.jsDate){
+                return false
+            } else{
+                return  String(n.jsDate) == String(value)
+            }
+        })
     },
 
     render: function() {
         var _this = this;
-//        console.log(this.props.sublist)
-        console.log("ALL DATA :",this.props.allData)
+        console.log("THE WORKER TEST ",this.props.worker)
+        console.log("ALL DATA :",this.props.alldata)
         return (React.createElement("div", null, 
         React.createElement("h1", null, "Date ", this.props.worker, " "), 
         React.createElement("h1", null, "Date ", this.props.dates[0].getFullYear(), " "), 
             React.createElement("ul", {className: "date-list"}, 
              this.props.dates.map(function(e, i){
 
-                console.log(_this.props.sublistfunc(e))
-                console.log("Calendar i ",i)
-                console.log("Calendar e ",e)
+                //console.log(_this.props.sublistfunc(e))
+                //console.log("Calendar i ",i)
+               // console.log("Calendar e ",e)
+                    var _i = i;
+                var _dateData = _this.dataByDate(_this.props.alldata, e);
+                console.log("The _i value is ",_i);
+                console.log("_dateData", _dateData);
+                console.log("_dateData", _dateData);
 
-                return (React.createElement("li", {className: "date-cells"}, React.createElement("p", null, String(e)), React.createElement(CalendarCell, {key: i, dateArray: _this.props.sublistfunc(e) || []})))
+                return (React.createElement("li", {className: "date-cells"}, React.createElement("p", null, String(e)), React.createElement(CalendarCell, {staticTester: "Just testing", key: "_i", dateArray: _dateData})))
                 }) 
                 )
         ));
@@ -34,11 +51,20 @@ AppMain.value('Calendar', Calendar);
 var CalendarCell = React.createClass({displayName: "CalendarCell",
     propTypes: {
         dateArray : React.PropTypes.array.isRequired,
-        key : React.PropTypes.number.isRequired
+        key : React.PropTypes.number,
+        staticTester : React.PropTypes.string
+    },
+
+    addHello:function(string){
+        console.log()
+        return string+" CalendarCell."
     },
 
     render: function() {
-        return (React.createElement("ul", {classname: "individual-leave-cell"}, this.props.dateArray.map(function(n){return React.createElement(WorkerLeaveCell, {key: n.key, data: n}) })));
+        console.log("CalendarCell key : ", this.props.key)
+        console.log("CalendarCell dateArray : ", this.props.dateArray)
+        console.log("CalendarCell staticTester : ", this.props.staticTester)
+        return (React.createElement("ul", {classname: "individual-leave-cell"}, this.addHello("helloo "), " ", this.props.dateArray.map(function(n){return React.createElement(WorkerLeaveCell, {key: n.key, data: n}) })));
     }
 });
 AppMain.value('CalendarCell', CalendarCell);
--- jsx/CalandarReactComponents.js
+++ jsx/CalandarReactComponents.js
@@ -1,28 +1,45 @@
 /** @jsx React.DOM */
 var Calendar = React.createClass({
     propTypes: {
-        allData : React.PropTypes.array,
+        alldata : React.PropTypes.array,
         dates : React.PropTypes.array.isRequired,
         sublist : React.PropTypes.array,
         daysoff : React.PropTypes.array,
-        sublistfunc : React.PropTypes.func
+        sublistfunc : React.PropTypes.func,
+        worker : React.PropTypes.string,
+        key:React.PropTypes.number
+    },
+
+    dataByDate: function(array, value ){
+        return _.filter(array, function(n){
+            if(!n.jsDate){
+                return false
+            } else{
+                return  String(n.jsDate) == String(value)
+            }
+        })
     },
 
     render: function() {
         var _this = this;
-//        console.log(this.props.sublist)
-        console.log("ALL DATA :",this.props.allData)
+        console.log("THE WORKER TEST ",this.props.worker)
+        console.log("ALL DATA :",this.props.alldata)
         return (<div>
         <h1>Date {this.props.worker} </h1>
         <h1>Date {this.props.dates[0].getFullYear()} </h1>
             <ul className="date-list">
             { this.props.dates.map(function(e, i){
 
-                console.log(_this.props.sublistfunc(e))
-                console.log("Calendar i ",i)
-                console.log("Calendar e ",e)
+                //console.log(_this.props.sublistfunc(e))
+                //console.log("Calendar i ",i)
+               // console.log("Calendar e ",e)
+                    var _i = i;
+                var _dateData = _this.dataByDate(_this.props.alldata, e);
+                console.log("The _i value is ",_i);
+                console.log("_dateData", _dateData);
+                console.log("_dateData", _dateData);
 
-                return (<li className="date-cells"><p>{String(e)}</p><CalendarCell key={i} dateArray ={_this.props.sublistfunc(e) || []} /></li>)
+                return (<li className="date-cells"><p>{String(e)}</p><CalendarCell staticTester="Just testing" key="_i" dateArray ={_dateData} /></li>)
                 }) }
                 </ul>
         </div>);
@@ -34,11 +51,20 @@ AppMain.value('Calendar', Calendar);
 var CalendarCell = React.createClass({
     propTypes: {
         dateArray : React.PropTypes.array.isRequired,
-        key : React.PropTypes.number.isRequired
+        key : React.PropTypes.number,
+        staticTester : React.PropTypes.string
+    },
+
+    addHello:function(string){
+        console.log()
+        return string+" CalendarCell."
     },
 
     render: function() {
-        return (<ul classname="individual-leave-cell">{this.props.dateArray.map(function(n){return <WorkerLeaveCell key={n.key}  data={n} /> })}</ul>);
+        console.log("CalendarCell key : ", this.props.key)
+        console.log("CalendarCell dateArray : ", this.props.dateArray)
+        console.log("CalendarCell staticTester : ", this.props.staticTester)
+        return (<ul classname="individual-leave-cell">{this.addHello("helloo ")} {this.props.dateArray.map(function(n){return <WorkerLeaveCell key={n.key}  data={n} /> })}</ul>);
     }
 });
 AppMain.value('CalendarCell', CalendarCell);
